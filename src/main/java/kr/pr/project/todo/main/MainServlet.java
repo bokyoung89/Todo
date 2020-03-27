0bc2f545d83b69d577625410b01fce516ff274e2
unknown <sbk8689@gmail.com>
2020-03-27 15:34:37 +0900

    MainServlet 생성 및 Database 연결

---
+++ src/main/java/kr/pr/project/todo/main/MainServlet.java
@@ -0,0 +1,36 @@
+package kr.pr.project.todo.main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.project.todo.dao.TodoDao;
+import kr.or.project.todo.dto.TodoDto;
+
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public MainServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		
+		TodoDao dao = new TodoDao();
+		List<TodoDto> list = dao.getTodos();
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println(list);
+		out.close();
+	 
+	}
+}
